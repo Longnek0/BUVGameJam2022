64c7b1b9df82ee4e9b4e8e38c3f18e1484963bc0
EssSohail <97175325+EssSohail@users.noreply.github.com>
2022-05-28 09:04:57 +0700

    push

--- Assets/GameJam/Scripts/Player.cs
+++ Assets/GameJam/Scripts/Player.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float Health;
+    public Text levelUp, maxLevel, livesUp;
+    GameObject a, b;
+
+    public int Health = 5;
+    public Rigidbody2D rb;
+    public float speed;
+
+    public bool rampageMode = false;
+    public AudioClip explode;
+
+    public AudioClip livesUp;
+    public AudioClip levelUp;
+
     // Start is called before the first frame update
     void Start()
     {
